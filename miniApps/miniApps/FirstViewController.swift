8c35e69d1fe844e67fbac8300d3ca0165d62fb05
Pablo Lomeli <pablolomeli@u.boisestate.edu>
2020-09-03 21:36:13 -0600

    adding base files and testing commit

---
+++ miniApps/miniApps/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  miniApps
+//
+//  Created by Pablo Lomeli on 9/3/20.
+//  Copyright © 2020 Pablo Lomeli. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ miniApps/miniApps/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  miniApps
+//
+//  Created by Pablo Lomeli on 9/3/20.
+//  Copyright © 2020 Pablo Lomeli. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
