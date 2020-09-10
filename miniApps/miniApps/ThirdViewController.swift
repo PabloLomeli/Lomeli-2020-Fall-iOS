150917462ad4ce929d3cf3a212479c1e2f7be4d4
Pablo Lomeli <pablolomeli@u.boisestate.edu>
2020-09-09 20:46:14 -0600

    Added third view to default code and made it a counter app.

---
+++ miniApps/miniApps/ThirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdViewController.swift
+//  miniApps
+//
+//  Created by Pablo Lomeli on 9/9/20.
+//  Copyright © 2020 Pablo Lomeli. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
