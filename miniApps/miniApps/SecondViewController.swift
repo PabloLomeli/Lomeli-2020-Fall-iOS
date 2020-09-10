34bc1674cd55fc8a70d6610d2ff29af0afac286e
Pablo Lomeli <pablolomeli@u.boisestate.edu>
2020-09-10 02:41:32 -0600

    added 13 more possible outcomes for RPL.

--- miniApps/miniApps/SecondViewController.swift
+++ miniApps/miniApps/SecondViewController.swift
@@ -17,7 +17,7 @@ class SecondViewController: UIViewController {
     var randNum2 : Int = 0
     
     @IBAction func player1Roll(_ sender: Any) {
-        randNum1 = Int.random(in: 1...3)
+        randNum1 = Int.random(in: 1...16)
         if randNum1 == 1 {
             player1Image.image = UIImage(named:"lightsaber.jpg")
         }
@@ -28,36 +28,48 @@ class SecondViewController: UIViewController {
             player1Image.image = UIImage(named:"paper.jpg")
         }
         if randNum1 == 4 {
-            player1Image.image = UIImage(named:"rock.png")
+            player1Image.image = UIImage(named:"banana.jpg")
         }
         if randNum1 == 5 {
-            player1Image.image = UIImage(named:"paper.jpg")
+            player1Image.image = UIImage(named:"blades of chaos.jpg")
         }
         if randNum1 == 6 {
-            player1Image.image = UIImage(named:"rock.png")
+            player1Image.image = UIImage(named:"bomb omb.jpg")
         }
         if randNum1 == 7 {
-            player1Image.image = UIImage(named:"paper.jpg")
+            player1Image.image = UIImage(named:"chainsaw gun.jpg")
         }
         if randNum1 == 8 {
-            player1Image.image = UIImage(named:"rock.png")
+            player1Image.image = UIImage(named:"cptshield.jpg")
         }
         if randNum1 == 9 {
-            player1Image.image = UIImage(named:"paper.jpg")
+            player1Image.image = UIImage(named:"diamond sword.png")
         }
         if randNum1 == 10 {
-            player1Image.image = UIImage(named:"paper.jpg")
+            player1Image.image = UIImage(named:"fortnite pickaxe.jpg")
         }
         if randNum1 == 11 {
-            player1Image.image = UIImage(named:"rock.png")
+            player1Image.image = UIImage(named:"keyblade.jpg")
         }
         if randNum1 == 12 {
-            player1Image.image = UIImage(named:"paper.jpg")
+            player1Image.image = UIImage(named:"laser rifle.jpg")
+        }
+        if randNum1 == 13 {
+            player1Image.image = UIImage(named:"plasma pistol.png")
+        }
+        if randNum1 == 14 {
+            player1Image.image = UIImage(named:"portal gun.png")
+        }
+        if randNum1 == 15 {
+            player1Image.image = UIImage(named:"raygun.jpg")
+        }
+        if randNum1 == 16 {
+            player1Image.image = UIImage(named:"mudkip.png")
         }
     }
     
     @IBAction func player2Roll(_ sender: Any) {
-        randNum2 = Int.random(in: 1...3)
+        randNum2 = Int.random(in: 1...16)
         if randNum2 == 1 {
             player2Image.image = UIImage(named:"lightsaber.jpg")
         }
@@ -68,31 +80,43 @@ class SecondViewController: UIViewController {
             player2Image.image = UIImage(named:"paper.jpg")
         }
         if randNum2 == 4 {
-            player2Image.image = UIImage(named:"lightsaber.jpg")
+            player2Image.image = UIImage(named:"banana.jpg")
         }
         if randNum2 == 5 {
-            player2Image.image = UIImage(named:"rock.png")
+            player2Image.image = UIImage(named:"blades of chaos.jpg")
         }
         if randNum2 == 6 {
-            player2Image.image = UIImage(named:"paper.jpg")
+            player2Image.image = UIImage(named:"bomb omb.jpg")
         }
         if randNum2 == 7 {
-            player2Image.image = UIImage(named:"lightsaber.jpg")
+            player2Image.image = UIImage(named:"chainsaw gun.jpg")
         }
         if randNum2 == 8 {
-            player2Image.image = UIImage(named:"rock.png")
+            player2Image.image = UIImage(named:"cptshield.jpg")
         }
         if randNum2 == 9 {
-            player2Image.image = UIImage(named:"paper.jpg")
+            player2Image.image = UIImage(named:"diamond sword.png")
         }
         if randNum2 == 10 {
-            player2Image.image = UIImage(named:"lightsaber.jpg")
+            player2Image.image = UIImage(named:"fortnite pickaxe.jpg")
         }
         if randNum2 == 11 {
-            player2Image.image = UIImage(named:"rock.png")
+            player2Image.image = UIImage(named:"keyblade.jpg")
         }
         if randNum2 == 12 {
-            player2Image.image = UIImage(named:"paper.jpg")
+            player2Image.image = UIImage(named:"laser rifle.jpg")
+        }
+        if randNum2 == 13 {
+            player2Image.image = UIImage(named:"plasma pistol.png")
+        }
+        if randNum2 == 14 {
+            player2Image.image = UIImage(named:"portal gun.png")
+        }
+        if randNum2 == 15 {
+            player2Image.image = UIImage(named:"raygun.jpg")
+        }
+        if randNum2 == 16 {
+            player2Image.image = UIImage(named:"mudkip.png")
         }
     }
     
