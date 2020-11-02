ffe488c856eba964498f4bc22749b1b8d6df53be
Pablo Lomeli <pablolomeli@Pablos-iMac.local>
2020-11-01 18:37:39 -0700

    added basic photo and map function

--- project1/Alone In Rads/Alone In Rads/ViewController.swift
+++ project1/Alone In Rads/Alone In Rads/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
-    var mainMenuArray:Array<String> = ["Play", "Set Avatar", "Settings", "Extras"]
+    var mainMenuArray:Array<String> = ["Play", "Set Avatar", "Start Location", "Settings", "Extras"]
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return mainMenuArray.count
@@ -34,7 +34,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
             tableView.deselectRow(at: indexPath, animated: false)
             if( indexPath.row == 1){
-                let detailsViewController = (storyboard?.instantiateViewController(identifier: "DetailsViewController"))!
+                let detailsViewController = (storyboard?.instantiateViewController(identifier: "PhotoViewController"))!
+                present(detailsViewController, animated: true, completion: nil)
+            }
+            else if( indexPath.row == 2){
+                let detailsViewController = (storyboard?.instantiateViewController(identifier: "MapViewController"))!
                 present(detailsViewController, animated: true, completion: nil)
             }
             else{
