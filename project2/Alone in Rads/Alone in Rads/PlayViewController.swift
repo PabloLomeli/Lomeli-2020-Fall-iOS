4b5f8cc006d4efae5467b93bf230c36514aac958
PabloLomeli <pablolomeli@u.boisestate.edu>
2020-11-24 21:15:51 -0700

    initial saving feature implemented

--- project2/Alone in Rads/Alone in Rads/PlayViewController.swift
+++ project2/Alone in Rads/Alone in Rads/PlayViewController.swift
@@ -8,23 +8,41 @@
 import UIKit
 import CoreData
 
-class PlayViewController: UIViewController {
+class PlayViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        saveTable.dataSource = self
+        saveTable.delegate = self
+        
+        do {
+            self.players = try context.fetch(PlayerData.fetchRequest())
+            
+            //reload save table
+            DispatchQueue.main.async {
+                self.saveTable.reloadData()
+            }
+        }
+        catch{
+            
+        }
+        
     }
     
+    @IBOutlet weak var saveTable: UITableView!
     @IBOutlet weak var playerName: UITextField!
     @IBOutlet weak var playerSex: UITextField!
     @IBOutlet weak var playerAge: UITextField!
     
     var currentG:[GameData]?
     var currentL:[LocationData]?
+    var players:[PlayerData]?
     
+    // confirm button logic
     @IBAction func savePlayerData(_ sender: Any) {
         let pName = playerName.text
         let pSex = playerSex.text
@@ -32,7 +50,7 @@ class PlayViewController: UIViewController {
         
         let currentPlayer = PlayerData(context: self.context)
         
-        // grab latest game data to use
+        // grab latest location data to use
         do {
             self.currentL = try context.fetch(LocationData.fetchRequest())
         }
@@ -64,14 +82,48 @@ class PlayViewController: UIViewController {
         let detailsViewController = (storyboard?.instantiateViewController(identifier: "SecondPlayViewController"))!
         present(detailsViewController, animated: true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    
+    // player saves logic
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return players?.count ?? 0
     }
-    */
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        
+        let save = self.players![indexPath.row]
+        
+        cell.textLabel?.text = save.playername
+        
+        print(indexPath.row)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 
+            tableView.deselectRow(at: indexPath, animated: false)
+            if( indexPath.row == 0){
+                
+            }
+            else if( indexPath.row == 1){
+                
+            }
+            else if( indexPath.row == 2){
+                
+            }
+            else if( indexPath.row == 3){
+                
+            }
+            else if( indexPath.row == 4){
+                
+            }
+        }
+
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 60
+        }
 }
--- project2/README.md
+++ project2/README.md
@@ -1,7 +1,11 @@
 # Project 2
 
-This is the second iteration of Alone in Rads that will feature using data core and some other requirements.
+This is the second iteration of Alone in Rads that will feature using data core, the other requirements, and hopefully extras if possible.
 
 So far, I've gotten the app icon to work, but the launch screen image is not working for some reason, but it's still set in xCode so I don't know after looking at a bunch of forums and suggestions.
 
 I then added core data entities and their relationships and just require to implement storing and retrieving the data that the player will input or use.
+
+I'm not sure how far we have to go into implementing each entitie and relationship we made for the core data requirement, but I've made use of it by storing a players name, then creating a game state that will save that, and also store the latest location available at character creation time. So a location isn't required, the game will tell you to set your location, but is entirely optional. No errors will be thrown at the user like it did to me when I was first debugging this.
+
+
